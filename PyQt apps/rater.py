48cea58c4f8a150a0bd3c047ebbea778436aac89
dogma03 <testarizona16@gmail.com>
2024-04-18 16:06:00 +0500

    rater

--- PyQt apps/rater.py
+++ PyQt apps/rater.py
@@ -21,6 +21,27 @@ v_line.addWidget(stroke)
 v_line.addWidget(rating)
 v_line.addWidget(rater)
 
+def rate():
+    password = stroke.text()
+    len_enough = len(password) > 8
+    has_uppercase = False
+    has_lowercase = False
+    has_numbers = False
+    has_symbols = False
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        if char.islower():
+            has_lowercase = True
+        if char.isdigit():
+            has_numbers = True
+        if char in string.punctuation:
+            has_numbers = True
+
+    rating1 = len_enough + has_numbers + has_lowercase + has_symbols + has_uppercase
+    rating.setText(f'Рейтинг: {'*' * rating1}')
+
+rater.clicked.connect(rate)
 
 window.setLayout(v_line)
 
