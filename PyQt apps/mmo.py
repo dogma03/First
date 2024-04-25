34dcfcad10b53217dba5873b539b617a440bfb17
dogma03 <testarizona16@gmail.com>
2024-04-25 15:18:29 +0500

    novie fichi

--- PyQt apps/mmo.py
+++ PyQt apps/mmo.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QListWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QListWidget, QTextEdit
 from PyQt5.QtCore import Qt
 
 # -----------------создание приложения и работа с окном-----------------------
@@ -14,26 +14,30 @@ game.show() # по умолчанию окно скрыто и нужно его
 # создание направляющих
 v_line = QVBoxLayout()
 h_line = QHBoxLayout()
+low_line = QHBoxLayout()
 
 inventory = QListWidget()
-window = QListWidget()
+information = QTextEdit()
 btn1 = QPushButton()
 btn2 = QPushButton()
 btn3 = QPushButton()
 btn4 = QPushButton()
 btn5 = QPushButton()
 
-v_line.addWidget(inventory)
-v_line.addWidget(window)
+h_line.addWidget(inventory)
+h_line.addWidget(information)
 
-v_line.addWidget(btn1)
-v_line.addWidget(btn2)
-v_line.addWidget(btn3)
-v_line.addWidget(btn4)
-v_line.addWidget(btn5)
+v_line.addLayout(h_line)
+
+low_line.addWidget(btn1)
+low_line.addWidget(btn2)
+low_line.addWidget(btn3)
+low_line.addWidget(btn4)
+low_line.addWidget(btn5)
+
+v_line.addLayout(low_line)
 
 # закрепление горизонтальной линии на вертикальной направляющей
-v_line.addLayout(h_line)
 
 # закрепление основного лэйаута для окна
 game.setLayout(v_line)
