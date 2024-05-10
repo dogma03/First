4e0e41ef8fbeb8c2b9d94c8145f8f0589aa4ce0f
dogma03 <testarizona16@gmail.com>
2024-05-10 18:11:14 +0500

    strashnie fichi(

--- kivy/main.py
+++ kivy/main.py
@@ -2,6 +2,9 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.clock import Clock
 
 from kivy.core.window import Window
 Window.size = (375, 650)
@@ -81,7 +84,59 @@ class SecondScreen(Screen):
         self.manager.current = 'main_menu'
 
 class CalcScreen(SecondScreen):
-    pass
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        axis = BoxLayout(
+            orientation = 'vertical',
+            padding = (50, 150, 50, 150)
+        )
+
+        self.result = Label(
+            text = '0',
+            size_hint_y = None,
+            height = 100,
+            font_size = 32
+        )
+        axis.add_widget(self.result)
+
+        keyboard = GridLayout(cols = 4)
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            'C', '0', '=', '+'
+        ]
+
+        for b in buttons:
+            temp = Button(
+                text = b,
+                font_size = 26,
+                on_press = self.keyboard_press
+            )
+            keyboard.add_widget(temp)
+
+        axis.add_widget(keyboard)
+
+        self.add_widget(axis)
+    
+    def keyboard_press(self, instance):
+        current = self.result.text
+        btn = instance.text
+
+        if current == '0' or current == 'Ошибка ало':
+            self.result.text = ''
+            current = ''
+
+        if btn == '=':
+            try:
+                self.result.text = str(eval(current))
+            except:
+                self.result.text = 'Ошибка ало'
+        elif btn == 'C':
+            self.result.text = '0'
+        else:
+            self.result.text = current + btn
+
 
 class ConvScreen(SecondScreen):
     pass
@@ -90,7 +145,71 @@ class TimerScreen(SecondScreen):
     pass
 
 class StopwatchScreen(SecondScreen):
-    pass
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+        self.time = 0
+        self.run = False
+
+        axis = BoxLayout(
+            orientation = 'vertical',
+            spacing = 10,
+            padding = (50, 150, 50, 150)
+        )
+
+        self.label = Label(
+            text = '00:00:00',
+            font_size = 32,
+        )
+        axis.add_widget(self.label)
+
+        self.start_btn = Button(
+            text = 'Запуск',
+            font_size = 32,
+            size_hint = (None, None),
+            size = (200, 50),
+            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            on_press = self.start_stop
+        )
+        axis.add_widget(self.start_btn)
+
+        self.reset_btn = Button(
+            text = 'Сбросить',
+            font_size = 32,
+            size_hint = (None, None),
+            size = (200, 50),
+            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+            on_press = self.reset
+        )
+        axis.add_widget(self.reset_btn)
+
+        self.add_widget(axis)
+
+
+        Clock.schedule_interval(self.update_time, 0.01)
+
+    def start_stop(self, instance):
+        self.run = not self.run
+        if self.run:
+            self.start_btn.text = 'Пауза'
+        else:
+            self.start_btn.text = 'Запуск'
+
+    def reset(self, instance):
+        self.run = False
+        self.time = 0
+        self.label.text = '00:00:00'
+        self.start_btn.text = 'Запуск'
+    
+    def format_time(self, ms):
+        s, ms = divmod(ms, 100)
+        m, s = divmod(s, 60)
+        return f'{m:02}:{s:02}:{ms:02}'
+
+    def update_time(self, dt):
+        if self.run:
+            self.time += 1
+        self.label.text = self.format_time(self.time)
 
 
 class MyApp(App):
